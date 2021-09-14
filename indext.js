3cda99f5285c372c4df01ad23ce9819e3e4842ce
sandhya bauddha <sandhyas19@navgurukul.org>
2021-09-14 22:24:45 +0530

    first commit

---
+++ indext.js
@@ -0,0 +1,40 @@
+const knex = require('knex')({
+    client: 'mysql',
+    connection: {
+        host: '127.0.0.1',
+        port: 3306,
+        user: 'root',
+        password: 'Sandhya@563',
+        database: 'sandy'
+    }
+});
+// console.log('hello')
+// this knex qureie for inserting data in tha database 
+knex('students').insert({ id: 3, name: 'sudha', description: 'navgurukul' })
+    .then((data) => {
+        console.log(data, 'data inserted')
+    }).catch((error) => {
+        console.log(error, 'error')
+    })
+// read the data from the database to get data
+knex.select('*').from('user')
+    .then((data) => {
+        // console.log(data);
+    })
+    .catch((err) => {
+        // console.log(err);
+    })
+// update the data from the database
+knex.update({ id: 1, name: 'anchal', description: 'delhi' })
+    .table('students').where("id", 1).then((data) => {
+        // console.log("data is updated");
+    }).catch((err) => {
+        // console.log("not updatate")
+    })
+knex('students').where('id').del()
+    .then((data) => {
+        // console.log('deleted the data');
+    }).catch((err) => {
+        // console.log('not delete');
+    })
+
